8e6faa488d9e6b948801e8c3977b60f3b2a6b476
Jared Reisinger <jaredreisinger@hotmail.com>
2013-10-18 22:45:07 -0700

    Initial IFF reader and dumping tool.

---
+++ IffDump/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+using TreatyOfBabel;
+
+namespace IffDump
+{
+    internal class Program
+    {
+        private class CommandLineArgs
+        {
+            [DefaultArgument(ArgumentType.Required,
+                HelpText = "The file to analyze.")]
+            public string InputFile = null;
+
+            [Argument(ArgumentType.Undocumented,
+                ShortName = "",
+                HelpText = "Pause before exiting.")]
+            public bool PauseBeforeExiting = false;
+
+
+            public string GetArgValues()
+            {
+                StringBuilder sb = new StringBuilder();
+
+                sb.AppendLine(string.Format("args: (InputFile): {0}", this.InputFile));
+
+                return sb.ToString();
+            }
+        }
+
+        static void ShowUsage()
+        {
+            Console.WriteLine(Parser.ArgumentsUsage(typeof(CommandLineArgs)));
+        }
+
+        private static void WaitToExit()
+        {
+            Console.WriteLine();
+            Console.Write("Press any key to exit...");
+            Console.ReadKey(true);
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program(args);
+            program.Run();
+        }
+
+        private string[] args;
+
+        private Program(string[] args)
+        {
+            this.args = args;
+        }
+
+        private void Run()
+        {
+            var parsedArgs = new CommandLineArgs();
+
+            try
+            {
+                if (!Parser.ParseArgumentsWithUsage(this.args, parsedArgs))
+                {
+                    throw new ArgumentException("** CommandLine.Parser **");
+                }
+
+                // analyze the file...
+                this.DumpIffFile(parsedArgs);
+            }
+            catch (Exception ex)
+            {
+                // Report it?
+                if (!(ex is ArgumentException && ex.Message == "** CommandLine.Parser **"))
+                {
+                    Console.WriteLine("EXCEPTION: {0}", ex.Message);
+                }
+
+                // auto-pause on exceptions if we're debugging.
+                if (System.Diagnostics.Debugger.IsAttached)
+                {
+                    parsedArgs.PauseBeforeExiting = true;
+                }
+            }
+            finally
+            {
+                if (parsedArgs.PauseBeforeExiting)
+                {
+                    WaitToExit();
+                }
+            }
+        }
+
+        private void DumpIffFile(CommandLineArgs commandLine)
+        {
+            // header info...
+            Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine("IffDump v0.1");
+            Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Getting IFF information from {0}...", commandLine.InputFile);
+
+            using (IffReader reader = new IffReader(commandLine.InputFile))
+            {
+                foreach (var chunk in reader.GetChunks(0))
+                {
+                    Console.WriteLine("chunk @0x{0:x8}, {1}, length 0x{2:x8}", chunk.Offset, chunk.TypeId, chunk.Length);
+
+                    if (chunk.TypeId == "FORM")
+                    {
+                        foreach (var chunkInner in reader.GetChunks(chunk.Offset + 4 + 4 + 4))
+                        {
+                            Console.WriteLine("--chunk @0x{0:x8}, {1}, length 0x{2:x8}", chunkInner.Offset, chunkInner.TypeId, chunkInner.Length);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- TreatyOfBabel/IffReader.cs
+++ TreatyOfBabel/IffReader.cs
@@ -2,11 +2,106 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace TreatyOfBabel
 {
-    public class IffReader
+    public class IffReader : IDisposable
     {
+        private BinaryReader reader;
+
+        public IffReader(string file)
+        {
+            this.reader = new BinaryReader(File.OpenRead(file));
+        }
+
+        public IffReader(BinaryReader reader)
+        {
+            this.reader = reader;
+        }
+
+        private bool disposed;
+        public void Dispose()
+        {
+            if (!this.disposed)
+            {
+                if (this.reader != null)
+                {
+                    this.reader.Dispose();
+                    this.reader = null;
+                }
+
+                this.disposed = true;
+            }
+        }
+
+        // Build a (nested?) tree of chunks...
+        private void ReadIff()
+        {
+
+        }
+
+        // Get chunks from the current offset, or offset passed in?
+        public IEnumerable<IffInfo> GetChunks(uint offset)
+        {
+            uint currentOffset = offset;
+            string typeId = "XXXX";
+
+            while (!string.IsNullOrEmpty(typeId))
+            {
+                // TODO: offsets must be even?  Add 1 if needed?
+                this.reader.BaseStream.Position = currentOffset;
+                typeId = this.ReadTypeId();
+
+                if (!string.IsNullOrEmpty(typeId))
+                {
+                    var length = this.reader.ReadBigEndianUint32();
+                    yield return new IffInfo(currentOffset, typeId, length);
+                    currentOffset += 4 + 4 + length;
+                }
+            }
+        }
+
+        private string ReadTypeId()
+        {
+            var chars = this.reader.ReadChars(4);
+
+            if (chars == null || chars.Length == 0)
+            {
+                return null;
+            }
+
+            System.Diagnostics.Debug.Assert(chars.Length == 4);
+            return new string(chars);
+        }
+    }
+
+    public class IffInfo
+    {
+        public IffInfo(uint offset, string typeId, uint length)
+        {
+            this.Offset = offset;
+            this.TypeId = typeId;
+            this.Length = length;
+        }
+
+        public uint Offset { get; private set; }
+        public string TypeId { get; private set; }  // add sub-type, for FORM?
+        public uint Length { get; private set; }
+    }
+
+    public static class BinaryReaderExtensions
+    {
+        public static uint ReadBigEndianUint32(this BinaryReader reader)
+        {
+            var bytes = reader.ReadBytes(4);
+
+            if (bytes == null || bytes.Length != 4)
+            {
+                throw new InvalidDataException("Expected to read at least 4 bytes!");
+            }
+
+            return ((uint)bytes[0] << 24) | ((uint)bytes[1] << 16) | ((uint)bytes[2] << 8) | ((uint)bytes[3]);
+        }
     }
 }
